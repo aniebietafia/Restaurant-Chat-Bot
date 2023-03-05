3c686ee9b2249aa04961be0c5cb7f71fe77839cf
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-05 21:36:06 +0100

    [Update] Socket.io connection

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 // Importing node core modules
 const path = require("path");
-const http = require("http");
+// const http = require("http");
 
 // Importing npm modules
 const express = require("express");
-const socketio = require("socket.io");
+const mongoose = require("mongoose");
+// const socketio = require("socket.io");
 require("dotenv").config();
 
 // Creating express app
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+//const server = http.createServer(app);
+//const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Setting up socket.io connection
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 // Creating server
-server.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
-});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((result) => {
+    // server.listen(PORT, () => {
+    //   console.log(`Server started on http://localhost:${PORT}`);
+    // });
+    const server = app.listen(PORT);
+    const socketio = require("socket.io")(server);
+    socketio.on("connection", (socket) => {
+      console.log("Client connected");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
