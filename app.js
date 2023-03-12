770c81fa5f80b02496ade24ac504a16e6be030fa
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-12 20:52:20 +0100

    Invoking the express app

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-// Importing node core modules
+// Import node core modules and npm packages
 const path = require("path");
-// const http = require("http");
-
-// Importing npm modules
 const express = require("express");
-require("dotenv").config();
 
-// Importing custom routes
-const productRoute = require("./routes/product.route");
+const sessionMiddleware = require("./middleware/session.middleware");
 
-const MONGODB_CONNECTION = require("./db/connect");
-
-// Creating express app and servers
+// Invoke express
 const app = express();
 
-// Set up static folder
-app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Order route middleware
-app.use(productRoute);
+app.use(sessionMiddleware);
 
-MONGODB_CONNECTION(process.env.MONGO_URI);
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}`);
-});
+module.exports = app;
