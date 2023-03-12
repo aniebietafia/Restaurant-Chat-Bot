cee213a525a388bf0e25e0c547d5c1fe202e9c55
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-12 20:54:49 +0100

    made vital changes

--- routes/product.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const productController = require("../controllers/product.controller");
-
-router.route("/products").get(productController.productsList).post(productController.createProduct);
-
-module.exports = router;
