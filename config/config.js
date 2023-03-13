6a8e683f2012f8c500c820912f5ea4cf94363435
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-13 14:12:51 +0100

    app configurations

---
+++ config/config.js
@@ -0,0 +1,18 @@
+const config = {
+  botName: process.env.botName,
+  PORT: process.env.PORT,
+  mongoURI: process.env.mongoURI,
+  sessionSecret: process.env.sessionSecret,
+  local_db: process.env.local_db,
+  db_name: process.env.db_name,
+  sessionMaxAge: process.env.sessionMaxAge,
+};
+
+const cors = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+module.exports = { config, cors };
