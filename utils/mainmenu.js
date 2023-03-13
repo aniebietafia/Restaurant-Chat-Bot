74087bb5707b9a3c383d4efa94e7a0a22daf697f
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-13 15:03:25 +0100

    available dishes in the restaurant

---
+++ utils/mainmenu.js
@@ -0,0 +1,22 @@
+const mainMenu = [
+  { number: 1, text: "Place An Order" },
+  { number: 99, text: "Checkout Order" },
+  { number: 98, text: "Check Order History" },
+  { number: 97, text: "Check Current Order" },
+  { number: 0, text: "Cancel Order" },
+];
+
+const foodMenu = [
+  { number: 1, food: "Egusi Soup" },
+  { number: 2, food: "Fried Plantain and Egg Sause" },
+  { number: 3, food: "Plantian & Beans" },
+  { number: 4, food: "Meat Sausage" },
+  { number: 5, food: "Fresh Fish Soup" },
+  { number: 6, food: "Coconut Rice" },
+  { number: 7, food: "Edikang Ikong Soup" },
+];
+
+module.exports = {
+  mainMenu,
+  foodMenu,
+};
