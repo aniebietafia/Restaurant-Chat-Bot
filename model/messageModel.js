c51892aabfbe20f30dbe397c8c2ae1e712416f39
AfiaAnieniet <leeboy4u2002@yahoo.com>
2023-03-13 14:59:16 +0100

    messages schema

---
+++ model/messageModel.js
@@ -0,0 +1,27 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+// import mongoose from 'mongoose';
+// const
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const MessageSchema = new Schema(
+	{
+		sessionID: {
+			type: String,
+			required: [true, "Enter a Session ID"],
+			trim: true,
+		},
+		userMessage: {
+			username: String,
+			text: String,
+			time: String,
+		},
+		botMessage: {
+			username: String,
+			text: String,
+			time: String,
+		},
+	},
+	{ timestamps: true }
+);
+module.exports = mongoose.model("Message", MessageSchema); 
